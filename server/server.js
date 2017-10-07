14967f593ab7cc5b7a10d0b29c0f096597eb81f6
Yannis Alexiou <johnmast7@yahoo.com>
2017-10-07 23:23:05 +0300

    Add GET /todos/:id

--- server/server.js
+++ server/server.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var bodyParser = require('body-parser');
+var {ObjectID} = require('mongodb');
 
 var {mongoose} = require('./db/mongoose');
 var {Todo} = require('./models/todo');
@@ -27,6 +28,42 @@ app.get('/todos', (req, res) => {
   }, (e) => {
     res.status(400).send(e);
   });
+});
+
+// GET /todos/1234324
+app.get('/todos/:id', (req, res) => {
+  //res.send(req.params);
+  var id = req.params.id;
+
+  if (!ObjectID.isValid(id)) {
+    return res.status(404).send();
+  }
+
+  Todo.findById(id).then((todo) => {
+    if (!todo) {
+      return res.status(404).send();
+    }
+
+    res.send({todo: todo});
+
+  }).catch((e) => {
+    res.status(400).send();
+  })
+
+  // Valid id using isValid
+    // 404 - send back empty send
+
+  // findById
+    // success
+      // if todo - send it back
+      // if not todo - send back 404 with empty body
+    // error
+      // 400 - and send empty body back
+
+
+
+
+
 });
 
 app.listen(3000, () => {
