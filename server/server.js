43377c26dfbeb85b8fcd0539b0ad56252c197dad
Yannis Alexiou <johnmast7@yahoo.com>
2017-10-07 16:43:35 +0300

    Add POST /todos route and refactor mangoose

--- server/server.js
+++ server/server.js
@@ -17,7 +17,7 @@ app.post('/todos', (req, res) => {
   todo.save().then((doc) => {
     res.send(doc);
   }, (e) => {
-    res.status(400).send(e);
+    res.status(400).send(e); //400 = bad request
   });
 });
 
