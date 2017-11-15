f1524b168258d893e8ecaaabb8b9a6853b17f1f1
Yannis Alexiou <johnmast7@yahoo.com>
2017-11-15 20:20:24 +0200

    minor changes

--- server/server.js
+++ server/server.js
@@ -160,10 +160,11 @@ app.get('/users/me', authenticate, (req, res) => {
 app.post('/users/login', (req, res) => {
   var body = _.pick(req.body, ['email', 'password']);
 
-  User.findByCredentials(body.email, body.password).then((user) => {
-    return user.generateAuthToken().then((token) => {
-      res.header('x-auth', token).send(user);
-    });
+  User.findByCredentials(body.email, body.password)
+    .then((user) => {
+      return user.generateAuthToken().then((token) => {
+        res.header('x-auth', token).send(user);
+      });
   }).catch((e) => {
     res.status(400).send();
   });
